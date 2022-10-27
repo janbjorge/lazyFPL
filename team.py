e5a779f4197ca56012d182288f37c483402aebf6
Jan Bjørge (mac) <janbjorge@gmail.com>
2022-10-27 19:14:50 +0200

    Show best lineup with show.py

--- team.py
+++ team.py
@@ -3,4 +3,7 @@ import helpers
 
 
 if __name__ == "__main__":
-    helpers.lprint(fetch.my_team())
+    helpers.lprint(
+        fetch.my_team(),
+        best=[p.name for p in helpers.best_lineup(fetch.my_team())],
+    )
