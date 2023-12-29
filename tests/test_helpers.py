5b752b666f7626e7c9e330874e602fcbc2d14a7c
JB Lovland <janbjorge@gmail.com>
2023-12-29 22:17:18 +0100

    Add helpers tests

---
+++ tests/test_helpers.py
@@ -0,0 +1,79 @@
+import pytest
+
+from lazyfpl import helpers, structures
+
+# Sample Player instances for testing with necessary attributes
+player1 = structures.Player(
+    fixutres=[],
+    name="Player1",
+    news="",
+    position="GKP",
+    price=25,
+    team="TeamA",
+    webname="Player1",
+    xP=1.0,
+)
+player2 = structures.Player(
+    fixutres=[],
+    name="Player2",
+    news="",
+    position="DEF",
+    price=50,
+    team="TeamA",
+    webname="Player2",
+    xP=2.0,
+)
+player3 = structures.Player(
+    fixutres=[],
+    name="Player3",
+    news="",
+    position="MID",
+    price=75,
+    team="TeamB",
+    webname="Player3",
+    xP=3.0,
+)
+player4 = structures.Player(
+    fixutres=[],
+    name="Player4",
+    news="",
+    position="FWD",
+    price=100,
+    team="TeamC",
+    webname="Player4",
+    xP=4.0,
+)
+lineup = [player1, player2, player3, player4]
+
+
+@pytest.mark.parametrize(
+    "lineup, expected_price",
+    [
+        (lineup, 250),
+        ([], 0),
+    ],
+)
+def test_squad_price(lineup: list[structures.Player], expected_price: int):
+    assert helpers.squad_price(lineup) == expected_price
+
+
+@pytest.mark.parametrize(
+    "lineup, expected_xp",
+    [
+        (lineup, 10.0),
+        ([], 0.0),
+    ],
+)
+def test_squad_xP(lineup: list[structures.Player], expected_xp: float):
+    assert helpers.squad_xP(lineup) == expected_xp
+
+
+@pytest.mark.parametrize(
+    "lineup, expected_overall_xp",
+    [
+        (lineup, pytest.approx(14.1421, 0.0001)),
+        ([], 0.0),
+    ],
+)
+def test_overall_xP(lineup: list[structures.Player], expected_overall_xp: float):
+    assert pytest.approx(helpers.overall_xP(lineup)) == expected_overall_xp
