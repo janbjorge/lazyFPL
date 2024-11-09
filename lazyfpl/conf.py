18b288a4ed3e85581caad4bcebd6398f30df9ff3
JB <janbjorge@gmail.com>
2024-11-09 12:14:44 +0100

    Use pydantic settings

--- lazyfpl/conf.py
+++ lazyfpl/conf.py
@@ -1,50 +1,50 @@
 from __future__ import annotations
 
-import os
 import pathlib
 import typing
 
-import pydantic
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
-class _Env(pydantic.BaseModel):
-    backtrace: int = pydantic.Field(
+class _Env(BaseSettings):
+    backtrace: int = Field(
         alias="FPL_BACKTRACE",
         default=3,
         gt=0,
     )
-    db: pathlib.Path = pydantic.Field(
+    db: pathlib.Path = Field(
         alias="FPL_DATABASE",
         default=pathlib.Path(__file__).parent.parent
         / pathlib.Path(".database.sqlite3"),
     )
-    debug: bool = pydantic.Field(
+    debug: bool = Field(
         alias="FPL_DEBUG",
         default=False,
     )
-    lookahead: int = pydantic.Field(
+    lookahead: int = Field(
         alias="FPL_LOOKAHEAD",
         default=3,
         gt=0,
     )
-    profile: str = pydantic.Field(
+    profile: str = Field(
         alias="FPL_PROFILE",
         default="",
     )
-    tabulate_format: str = pydantic.Field(
+    tabulate_format: str = Field(
         alias="FPL_TABULATE_FORMAT",
         default="tsv",
     )
-    teamid: str = pydantic.Field(
+    teamid: str = Field(
         alias="FPL_TEAMID",
         default="",
     )
 
 
-backtrace: typing.Final = _Env.model_validate(os.environ).backtrace
-db: typing.Final = _Env.model_validate(os.environ).db
-debug: typing.Final = _Env.model_validate(os.environ).debug
-lookahead: typing.Final = _Env.model_validate(os.environ).lookahead
-profile: typing.Final = _Env.model_validate(os.environ).profile
-tabulate_format: typing.Final = _Env.model_validate(os.environ).tabulate_format
-teamid: typing.Final = _Env.model_validate(os.environ).teamid
+backtrace: typing.Final = _Env().backtrace
+db: typing.Final = _Env().db
+debug: typing.Final = _Env().debug
+lookahead: typing.Final = _Env().lookahead
+profile: typing.Final = _Env().profile
+tabulate_format: typing.Final = _Env().tabulate_format
+teamid: typing.Final = _Env().teamid
